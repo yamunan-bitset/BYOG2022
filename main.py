79b6095c15a5e0275b597930cd8aa02232a7410c
ARCSCH(X) <yamunan.bitset@gmail.com>
2022-08-26 18:32:10 +0200

    added light and improved mirror

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ pygame.display.set_caption("Light")
 clock = pygame.time.Clock()
 done = False
 dt = pygame.time.get_ticks()
-rects = []
+circs = []
 hold = False
 pos = (random.randint(100, 900), random.randint(100, 700))
+light = [(pos[0], pos[1])]
 while not done:
     clock.tick(60)
     for event in pygame.event.get():
@@ -16,16 +17,23 @@ while not done:
             done = True
             break
         if event.type == pygame.MOUSEBUTTONDOWN:
-            hold = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            hold = False
-    if hold:
-        rects.append(pygame.Rect(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 10, 10))
+            circs.append(pygame.mouse.get_pos())
     screen.fill((170, 140, 0))
     pygame.draw.circle(screen, (200, 100, 240), pos, 50)
     pygame.draw.circle(screen, (240, 170, 250), pos, 25)
-    for rect in rects:
-        pygame.draw.rect(screen, (200, 200, 200), rect)
+    light.append((light[-1][0]+1, light[0][1]))
+    i = 0
+    for circ in circs:
+        i += 1
+        pygame.draw.circle(screen, (200, 200, 200), circ, 30)
+        pygame.draw.circle(screen, (230, 230, 230), circ, 15)
+        if i == 2:
+            pygame.draw.line(screen, (210, 210, 210), circ, circ_, 3)
+            i = 0
+        else:
+            circ_ = circ
+    for lig in light:
+        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(lig[0], lig[1], 10, 10))
     display.blit(screen, (0, 0))
     pygame.display.update()
 pygame.quit()
